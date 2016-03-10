5d5745d6458380ef528949ebd4d5062922829a1d
Zhiwei Chen <z.w.chan.jason@gmail.com>
2016-03-09 20:17:54 -0500

    parse criteria by synonym only

--- src/main/scala/com/votors/umls/AnalyzeCT.scala
+++ src/main/scala/com/votors/umls/AnalyzeCT.scala
@@ -4,6 +4,7 @@ import java.io.{FileWriter, PrintWriter, FileReader}
 import com.votors.common.Conf
 import com.votors.common.Utils.Trace._
 import org.apache.commons.csv._
+import scala.actors.threadpool.AtomicInteger
 import scala.collection.mutable.ListBuffer
 
 import scala.collection.JavaConversions.asScalaIterator
@@ -20,13 +21,14 @@ import scala.io.Codec
 
 case class CTRow(val tid: String, val criteriaType:String, var sentence:String, var markedType:String="", var depth:String="-1", var cui:String="None", var cuiStr:String="None"){
   var hitNumType = false
+  val sentId = AnalyzeCT.sentIdIncr.getAndIncrement()
   override def toString(): String = {
-    val str = f""""${tid.trim}","${criteriaType.trim}","${sentence.trim.replaceAll("\\\"","'")}","${markedType}","${depth}","${cui}","${cuiStr}"""" + "\n"
+    val str = f""""${tid.trim}","${criteriaType.trim}","${markedType}","${depth}","${cui}","${cuiStr}","${sentId}","${sentence.trim.replaceAll("\\\"","'")}"""" + "\n"
     if(markedType.size > 1 && markedType != "None")trace(INFO, "Get CTRow parsing result: " + str)
     str
   }
   def getTitle(): String = {
-    """"tid","type","sentence","Numerical type","depth" ,"cui" ,"cuiStr"""" + "\n"
+    """"tid","type","Numerical type","depth" ,"cui" ,"cuiStr","sentence_id","sentence"""" + "\n"
   }
 }
 
@@ -40,11 +42,17 @@ class AnalyzeCT(csvFile: String, outputFile:String, externFile:String, externRet
   val STAG_INCLUDE=1
   val STAG_EXCLUDE=2
   val STAG_BOTH=3
+  val STAG_DISEASE_CH=4
+  val STAG_PATIENT_CH=5
+  val STAG_PRIOR_CH  =6
 
   val TYPE_INCLUDE = "Include"
   val TYPE_EXCLUDE = "Exclude"
   val TYPE_BOTH = "Both"
   val TYPE_HEAD = "Head"
+  val TYPE_DISEASE_CH = "DISEASE CHARACTERISTICS"
+  val TYPE_PATIENT_CH = "PATIENT CHARACTERISTICS"
+  val TYPE_PRIOR_CH = "PRIOR CONCURRENT THERAPY"
   val TYPE_INCLUDE_HEAD = "Include head"
   val TYPE_EXCLUDE_HEAD = "Exclude head"
   val TYPE_BOTH_HEAD = "Both head"
@@ -126,18 +134,27 @@ class AnalyzeCT(csvFile: String, outputFile:String, externFile:String, externRet
       val criteria = row.get(1)
 
       var stagFlag = STAG_HEAD
-      criteria.split("#").foreach(sent_org =>{
+      criteria.split("#|\\n").foreach(sent_org =>{
         val sent = sent_org.trim.replaceAll("^\\p{Punct}*","")
         val ctRow =
-          if (stagFlag == STAG_HEAD && sent.toUpperCase.contains("INCLUSION CRITERIA")){
+          if (stagFlag != STAG_INCLUDE && sent.toUpperCase.startsWith("INCLUSION CRITERIA")){
             stagFlag = STAG_INCLUDE
             CTRow(tid,TYPE_INCLUDE_HEAD,sent)
-          }else if (stagFlag == STAG_HEAD && sent.toUpperCase.contains("INCLUSION AND EXCLUSION CRITERIA")) {
-            stagFlag = STAG_BOTH
-            CTRow(tid,TYPE_BOTH_HEAD,sent)
-          }else if ((stagFlag == STAG_HEAD || stagFlag == STAG_INCLUDE) && sent.toUpperCase.contains("EXCLUSION CRITERIA")) {
+          }else if (stagFlag != STAG_EXCLUDE && sent.toUpperCase.startsWith("EXCLUSION CRITERIA")) {
             stagFlag = STAG_EXCLUDE
             CTRow(tid,TYPE_EXCLUDE_HEAD,sent)
+          }else if (stagFlag != STAG_INCLUDE && stagFlag != STAG_EXCLUDE && stagFlag != STAG_DISEASE_CH && sent.toUpperCase.startsWith("DISEASE CHARACTERISTICS")) {
+            stagFlag = STAG_DISEASE_CH
+            CTRow(tid,TYPE_DISEASE_CH,sent)
+          }else if (stagFlag != STAG_INCLUDE && stagFlag != STAG_EXCLUDE  && stagFlag != STAG_PATIENT_CH && sent.toUpperCase.startsWith("PATIENT CHARACTERISTICS")) {
+            stagFlag = STAG_PATIENT_CH
+            CTRow(tid,TYPE_PATIENT_CH,sent)
+          }else if (stagFlag != STAG_INCLUDE && stagFlag != STAG_EXCLUDE && stagFlag != STAG_PRIOR_CH && sent.toUpperCase.startsWith("PRIOR CONCURRENT THERAPY")) {
+            stagFlag = STAG_PRIOR_CH
+            CTRow(tid,TYPE_PRIOR_CH,sent)
+          }else if (stagFlag == STAG_HEAD && sent.toUpperCase.startsWith("INCLUSION AND EXCLUSION CRITERIA")) {
+            stagFlag = STAG_BOTH
+            CTRow(tid,TYPE_BOTH_HEAD,sent)
           }else {
             stagFlag match {
               case STAG_HEAD => {
@@ -152,6 +169,18 @@ class AnalyzeCT(csvFile: String, outputFile:String, externFile:String, externRet
               case STAG_EXCLUDE => {
                 CTRow(tid, TYPE_EXCLUDE, sent)
               }
+              case STAG_DISEASE_CH => {
+                CTRow(tid, TYPE_DISEASE_CH, sent)
+              }
+              case STAG_PATIENT_CH => {
+                CTRow(tid, TYPE_PATIENT_CH, sent)
+              }
+              case STAG_PRIOR_CH => {
+                CTRow(tid, TYPE_PRIOR_CH, sent)
+              }
+              case _ =>
+                CTRow(tid, "None", sent)
+
             }
           }
         //detectQuantity(ctRow,writer)
@@ -209,12 +238,13 @@ class AnalyzeCT(csvFile: String, outputFile:String, externFile:String, externRet
   }
 
   // detect normal key words
+  var multiHit = true
   def detectKeyword(ctRow: CTRow, writer:PrintWriter) = {
     //replace all table,  reduce to only one space between words
     initKeyword()
     ctRow.sentence = ctRow.sentence.replaceAll("\\s+"," ")
     keywords.foreach(kw=>{
-      if(ctRow.markedType.size ==0 && ctRow.sentence.toLowerCase.matches(s".*\\b${kw._4}\\b.*")){
+      if((multiHit || ctRow.markedType.size ==0) && ctRow.sentence.toLowerCase.matches(s".*\\b${kw._4}\\b.*")){
         //println(s"$kw, $ctRow")
         ctRow.markedType = kw._1
         ctRow.depth = kw._2
@@ -280,13 +310,47 @@ class AnalyzeCT(csvFile: String, outputFile:String, externFile:String, externRet
     })
   }
 
+  /**
+   * only find the synonym of the current keyword
+   * @param cui
+   * @param term
+   * @param dept
+   */
+  def findChildTerm_simple(cui: String, term: String, dept: Int): Unit = {
+    val ret1 = tagger.execUpdate("drop table if exists prtbl;")
+    val ret2 = tagger.execUpdate("create /*temporary*/ table prtbl as (" +
+        "  select distinct cui from umls.mrconso " +
+        s"  where str='${term}' and LAT='ENG' " +
+        " );" )
+
+    val ret = tagger.execQuery("select distinct conso.cui, conso.str from umls.mrconso as conso, prtbl as pr where conso.cui=pr.cui;")
+    val buff = new ArrayBuffer[(String,String)]()
+    while(ret.next()) {
+      if (hCriteria.contains((ret.getString(1), ret.getString(2)))) {
+        println(s"${ret.toString} already exists.")
+      }else {
+        buff.append((ret.getString(1), ret.getString(2)))
+      }
+    }
+    println(s"dept: ${dept}, ${cui}, ${term}: get child number ${buff.size}")
+
+    ret.close()
+    buff.foreach(kv =>{
+      hCriteria.getOrElseUpdate((kv._1,kv._2),(dept))
+      //findChildTerm(kv._1, kv._2, dept+1)
+    })
+  }
+
+
   def findTerm(): Unit = {
     var writer = new PrintWriter(new FileWriter(externRetFile))
     Source.fromFile(externFile).getLines().foreach(line => {
       val tmp = line.split(",",2)
       if (tmp.size>1) {
-        findChildTerm(tmp(0),tmp(1),0)
-
+        //findChildTerm(tmp(0),tmp(1),0)
+        findChildTerm_simple(tmp(0),tmp(1),1)
+        // if find nothing in umls, just search itself
+        if (hCriteria.size == 0) hCriteria.getOrElseUpdate((tmp(0),tmp(1)),0)
         writer.append(s"#,${tmp(0)},${tmp(1)}\n")
         hCriteria.foreach(term => {
           writer.append(s"${term._2},${term._1._1},${term._1._2}\n")
@@ -294,28 +358,35 @@ class AnalyzeCT(csvFile: String, outputFile:String, externFile:String, externRet
         hCriteria.clear()
       }
     })
-
-
     writer.close()
   }
 }
 
 object AnalyzeCT {
-  def doGetKeywords(f: String, externFile:String) = {
-    val ct = new AnalyzeCT(s"C:\\fsu\\ra\\data\\201601\\split_criteria\\${f}.csv",
-      s"C:\\fsu\\ra\\data\\201601\\split_criteria\\${f}_ret.csv",
-      s"C:\\fsu\\ra\\data\\201601\\split_criteria\\${externFile}.txt",
-      s"C:\\fsu\\ra\\data\\201601\\split_criteria\\${externFile}_ret.txt")
+  val sentIdIncr = new AtomicInteger()
+
+  def doGetKeywords(dir:String, f: String, externFile:String) = {
+    val ct = new AnalyzeCT(s"${dir}${f}.csv",
+      s"${dir}${f}_ret.csv",
+      s"${dir}${externFile}.txt",
+      s"${dir}${externFile}_ret.txt")
     //ct.analyzeFile()
     ct.findTerm()
   }
-  def doAnaly(f: String, externFile:String): Unit = {
-    val ct = new AnalyzeCT(s"C:\\fsu\\ra\\data\\201601\\split_criteria\\${f}.csv",
-      s"C:\\fsu\\ra\\data\\201601\\split_criteria\\${f}_ret.csv",
-      s"C:\\fsu\\ra\\data\\201601\\split_criteria\\${externFile}.txt",
-      s"C:\\fsu\\ra\\data\\201601\\split_criteria\\${externFile}_ret.txt")
+  def doAnaly(dir:String, f: String, externFile:String): Unit = {
+    val ct = new AnalyzeCT(s"${dir}${f}.csv",
+      s"${dir}${f}_ret.csv",
+      s"${dir}${externFile}.txt",
+      s"${dir}${externFile}_ret.txt")
     ct.analyzeFile()
   }
+
+  /**
+   * arg 1: dir
+   * arg 2: file to be parsed, no ext; separated with ',', end with '/' or '\'; (ext is csv)
+   * arg 3: extern input fil, no ext;  (ext is txt)
+   * @param avgs
+   */
   def main(avgs: Array[String]): Unit = {
 //    doAnaly("Obesity_05_14_random300")
 //    doAnaly("Congective_Heart_Failure_05_14_all233")
@@ -328,7 +399,25 @@ object AnalyzeCT {
 //    doAnaly("Dementia_05_14_all197","criteriaWords")
 //    doAnaly("Hypertension_05_14_random300","criteriaWords")
 //    doAnaly("T2DM_05_14_random300","criteriaWords")
-      doAnaly("criteria_cancer_trials_2004_2014","criteriaWords")
+      //doAnaly("criteria_cancer_trials_2004_2014","criteriaWords")
 //    doGetKeywords("T2DM_05_14_random300","criteriaWords")
+    println("the input args are:\n" + avgs.mkString("\n"))
+    if (avgs.size < 4) {
+      println(s"invalid inputs, should be: prepare|parse dir file1,file2... extern-file")
+      sys.exit(1)
+    }
+    val jobType = avgs(0)
+    val dir = avgs(1)
+    val iFiles = avgs(2).split(",")
+    val extFile = avgs(3)
+
+    if (jobType == "prepare")doGetKeywords(dir, extFile, extFile)
+
+    iFiles.foreach(f =>{
+      println(s"processing: ${f}")
+      if (jobType == "parse")doAnaly(dir, f, extFile)
+    })
+
+
   }
 }
