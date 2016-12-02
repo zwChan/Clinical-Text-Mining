3f7e444c1b0da5c979b1704663a6e71c87ddc327
Zhiwei Chen <z.w.chan.jason@gmail.com>
2016-12-02 01:30:49 -0500

    get clinical trials id from web using python

---
+++ py/pre_run.py
@@ -0,0 +1,22 @@
+﻿#!/usr/bin/python
+
+'''
+ Prepare the data for the project, for now, just unzip the files.
+ I don't find the way to find the exact type of the file, but I know it can be decompressed by gzip.
+ So I call the shell command "gzip -d -f" to do it.
+'''
+import os
+import gzip  
+
+def walk_dir(dir,topdown=True):
+    for root, dirs, files in os.walk(dir, topdown):
+        for name in files:
+            print name
+            if not name.endswith(".Z"): continue
+            print("unzip file: " + os.path.join(root,name))
+            os.system("gzip -d -f " + os.path.join(root,name))
+           
+
+walk_dir("./data")
+
+
