714fd1e506b70980fadced8880521ee68e662429
Zhiwei Chen <z.w.chan.jason@gmail.com>
2017-05-23 14:30:55 -0400

    update sql

---
+++ sql-script/socialqa.sql
@@ -0,0 +1,5 @@
+
+select count(*) from socialqa.qdataH; -- 3822256
+select distinct top_level_category from socialqa.qdataH; -- 29
+select count(*) from socialqa.qdataH where top_level_category='Health'; -- 2820179
+
--- sql-script/synonym.sql
+++ sql-script/synonym.sql
@@ -9,6 +9,7 @@ synonym text
 set group_concat_max_len=10240;
 select * from test_term_umls;
 -- synonym with at most 3 words
-update test_term_umls t set synonym = (select GROUP_CONCAT(distinct s.descr_stemmed SEPARATOR  '|' ) from umls._target_term_ s where cui=t.cui and (length(descr_stemmed)-length(replace(descr_stemmed,' ', '')))<= 2);
+update test_term_umls t set synonym = (select GROUP_CONCAT(distinct s.descr SEPARATOR  '|' ) from umls._target_term_ s where cui=t.cui and (length(descr)-length(replace(descr,' ', '')))<= 2);
 
-select GROUP_CONCAT(distinct s.str SEPARATOR  ' | ' ) from umls.mrconso s, test_term_umls t where cui=t.cui and (length(str)-length(replace(str,' ', '')))<= 2 group by t.cui;
+
+select * from umls._target_term_ where cui='C2349972';
