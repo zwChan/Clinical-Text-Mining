d70810aebf66c814bf616c6ea733c97900f3d824
Zhiwei Chen <z.w.chan.jason@gmail.com>
2017-05-31 11:46:35 -0400

    update sql file

--- sql-script/socialqa.sql
+++ sql-script/socialqa.sql
@@ -5,8 +5,11 @@ select count(*) from socialqa.qdataH; -- 3822256
 select distinct top_level_category from socialqa.qdataH; -- 29
 select count(*) from socialqa.qdataH where top_level_category='Health'; -- 2820179
 
+create table health_answers_for_8000_questions as (select a.qid,a.content,a.rating, a.userid,a.usernick from adataH a, health_questions_random_8000 q where a.qid=q.qid);
 select * from socialqa.qdataH;
+select count(*) from `health_questions_random_8000`;
 
+-- split all answers into multiple files.
 select  id, replace(concat(subject, ' ', content,' ',chosenanswer),'\r','') from socialqa.qdataH 
     where id > 3822256/4*0 and id <= 3822256/4*1
 	into outfile '/tmp/socialqa_dataset1.csv' fields terminated by ',' enclosed by '"' lines terminated by '\n';
--- sql-script/synonym.sql
+++ sql-script/synonym.sql
@@ -13,10 +13,13 @@ rename table socialqa.wiki_ngram to synonym.wiki_ngram;
 set group_concat_max_len=10240;
 select * from test_term_umls;
 -- synonym with at most 3 words
+truncate test_term_umls;
+insert into test_term_umls (term,cui) select distinct ngram, cui_umls from wiki_ngram;
 update test_term_umls t set synonym = (select GROUP_CONCAT(distinct s.descr SEPARATOR  '|' ) from umls._target_term_ s where cui=t.cui and (length(descr)-length(replace(descr,' ', '')))<= 2);
 
 
 select * from umls._target_term_ where cui='C0439234';
 select * from umls.mrconso where cui='C0439234';
-
+select count(*) from  wiki_ngram;
+select * from test_term_umls;
 
