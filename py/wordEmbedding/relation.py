8a98ac1c4fe61e8f52552b7ad2e2301beafa356d
Zhiwei Chen <z.w.chan.jason@gmail.com>
2017-06-15 02:08:30 -0400

    embedding term evaluation on wordNet lations

---
+++ py/wordEmbedding/relation.py
@@ -0,0 +1,164 @@
+from __future__ import division
+
+__author__ = 'Jason'
+import sys
+import csv
+import  gensim
+
+class Evaluate:
+    """
+    ['term name','term_norm', 'umls_syn','wn_syn','wn_ant','wn_hyper','wn_hypo','wn_holo','wn_mero','wn_sibling']
+    """
+    def __init__(self,termList, topn=10):
+        if len(termList) == 0:
+            print("No term found.")
+            return
+        self.termList = termList
+        self.topn = topn
+        self.termNum = len(termList)
+        self.relName = self.termList[0].relName
+        self.NameIndex = self.termList[0].NameIndex
+        self.NormIndex = self.termList[0].NormIndex
+        self.cnt = self.empty_ret() # occurring count of the relation words
+        self.hit_cnt = self.empty_ret() #total of hit relation words
+        self.cnt_term = self.empty_ret() # number of terms that has this relation word
+        self.hit_term = self.empty_ret() # number of terms that is hit this relation word
+        self.hit_cnt_weighted = self.empty_ret() # weighted hit number of relation word. weight = 1 / min(number of relation word, topn)
+
+        # self.evaluate()
+
+    def empty_ret(self):
+        ret = []
+        for i in range(0,len(self.relName)):
+            ret.append(0)
+        return ret
+
+    def evaluate(self):
+        for term in self.termList:
+            for i,rels in enumerate(term.relValue):
+                if i <= term.NormIndex: continue  # skip term name and term norm
+                if len(rels) > 0:
+                    self.cnt_term[i] += 1
+                    self.hit_cnt_weighted[i] += 1.0*len(term.relHit[i])/min([len(rels),self.topn])
+                self.cnt[i] += len(rels)
+                if len(term.relHit[i]):
+                    self.hit_term[i] += 1
+                self.hit_cnt[i] += len(term.relHit[i])
+
+    def __str__(self):
+        ret = 'Evaluation:\t%s\n' % ('\t'.join([str(i) for i in self.relName[self.NormIndex+1:]]))
+        ret += "cnt:\t%s\n" % ('\t'.join([str(i) for i in self.cnt[self.NormIndex+1:]]))
+        ret += "cnt_term:\t%s\n" % ('\t'.join([str(i) for i in self.cnt_term[self.NormIndex+1:]]))
+        ret += "cnt_avg:\t%s\n" % ('\t'.join(['%0.2f' % (z[0]/z[1] if z[1]!=0 else 0) for z in zip(self.cnt,self.cnt_term)[self.NormIndex+1:]]))
+        ret += "hit_cnt:\t%s\n" % ('\t'.join([str(i) for i in self.hit_cnt[self.NormIndex+1:]]))
+        ret += "hit_term:\t%s\n" % ('\t'.join([str(i) for i in self.hit_term[self.NormIndex+1:]]))
+        ret += "hit_weight:\t%s\n" % ('\t'.join(["%0.2f" % (i) for i in self.hit_cnt_weighted[self.NormIndex+1:]]))
+        ret += "hit_cnt_avg:\t%s\n" % ('\t'.join(['%0.3f' % (z[0]/z[1] if z[1]!=0 else 0) for z in zip(self.hit_cnt,self.hit_term)[self.NormIndex+1:]]))
+        ret += "hit_cnt_pct:\t%s\n" % ('\t'.join(['%0.4f' % (100*z[0]/z[1] if z[1]!=0 else 0) for z in zip(self.hit_cnt,self.cnt)[self.NormIndex+1:]]))
+        ret += "hit_cnt_term_pct:\t%s\n" % ('\t'.join(['%0.4f' % (100*z[0]/z[1] if z[1]!=0 else 0) for z in zip(self.hit_term,self.cnt_term)[self.NormIndex+1:]]))
+        ret += "hit_weight_term_pct:\t%s\n" % ('\t'.join(['%0.4f' % (100*z[0]/z[1] if z[1]!=0 else 0) for z in zip(self.hit_cnt_weighted,self.cnt_term)[self.NormIndex+1:]]))
+        ret += "\n"
+        return ret
+
+    def PrintHitList(self):
+        print('\t'.join(self.relName))
+        for t in self.termList:
+            print(t)
+
+class Term:
+    """
+    ['term name','term_norm', 'umls_syn','wn_syn','wn_ant','wn_hyper','wn_hypo','wn_holo','wn_mero','wn_sibling']
+    """
+    relName = []
+    NameIndex = 0
+    NormIndex = 1
+
+    def __init__(self, term=''):
+        self.name = term # name of the term, e.g. Love (N)
+        self.relValue = [] # relation word of the term. e.g. synonym, antonym, ...
+        self.relHit = []   # the relation word is hit in the nearest neighbors.
+        self.similar = []  # similar terms retrieved from embedding vectors
+    def __str__(self, simple=True):
+        ret = ''
+        if simple:
+            ret += "%s\t" % ('\t'.join(['|'.join(t) for t in self.relValue[0:self.NormIndex+1]]))
+            ret += "%s" % ('\t'.join([ '|'.join(t) for t in self.relHit[self.NormIndex+1:]]))
+        else:
+            ret += 'name:\t%s, norm:%s\n' % (self.name,self.relValue[self.NormIndex][0])
+            ret += 'similar:\t'
+            for sim in self.similar:
+                ret += "(%s,%0.2f), " % sim
+            ret += '\n'
+            for i,title in enumerate(self.relName):
+                if i < self.NormIndex: continue
+                ret += "%s:\t%s\n" % (title,' '.join(self.relHit[i]))
+            ret += '\n'
+        return ret
+    def similarMap(self):
+        simMap = {}
+        for kv in self.similar:
+            simMap[kv[0]] = kv[1]
+        return simMap
+
+def term_similar(w2v, term,  most_similar_f, topn,restrict_vocab=30000):
+    term_norm = term.relValue[term.NormIndex]
+    sim = []
+    try:
+        term.similar = most_similar_f(w2v,term_norm,topn=topn,restrict_vocab=restrict_vocab)
+    except KeyError as e:
+        print("No such word: %s" % (term_norm))
+    simMap = term.similarMap()
+
+    for rels in term.relValue:
+        hitTerms = []
+        for rel in rels:
+            if rel in simMap:
+                hitTerms.append(rel)
+        term.relHit.append(hitTerms)
+
+def accuracy_rel(w2v, csvfile,most_similar_f, topn=10, restrict_vocab=30000,case_insensitive=True):
+    termList = []
+    with open(csvfile, 'rb') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter='\t', quotechar='"')
+        first = True
+        for row in csvreader:
+            if len(row) < 2: continue
+            if first:
+                first = False
+                Term.relName += row
+                continue
+            term = Term(row[0])
+            for i,rel in enumerate(row):
+                term.relValue.append([])
+                # if len(rel)==0: continue
+                rel_term = rel.split('|')
+                for t in rel_term:
+                    if len(t) == 0: continue
+                    term.relValue[i].append(t)
+            term_similar(w2v,term,most_similar_f,topn,restrict_vocab)
+            termList.append(term)
+    return termList
+
+
+if len(sys.argv) < 4:
+    print("Usage: [model-file] [vocab-file] [input-file]")
+model = sys.argv[1]
+model = r'C:\fsu\class\thesis\token.txt.bin'
+vocFile=sys.argv[2]
+vocFile = r'C:\fsu\class\thesis\token.txt.voc'
+infile = sys.argv[3]
+qfile = r'C:\fsu\ra\data\201706\synonym_ret.csv'
+wv = gensim.models.KeyedVectors.load_word2vec_format(model,fvocab=vocFile,binary=True,encoding='ascii', unicode_errors='ignore')
+# wv.most_similar_cosmul(['king','women'],['man'])
+# wv.accuracy(qfile)
+topn = 10
+termList = accuracy_rel(wv,qfile,gensim.models.KeyedVectors.most_similar,topn=topn)
+evaluation = Evaluate(termList,topn=topn)
+print("### result start: ###")
+evaluation.PrintHitList()
+print("#### evaluation result ###")
+evaluation.evaluate()
+print(evaluation)
+
+
+
--- py/wordEmbedding/wordnet_relation.py
+++ py/wordEmbedding/wordnet_relation.py
@@ -1,5 +1,6 @@
 __author__ = 'Jason'
-import csv,sys,re
+import csv,sys,re,json,yaml
+from stanfordcorenlp import StanfordCoreNLP
 
 ################
 # Obtain synonym from wordnet. docs: http://www.nltk.org/howto/wordnet.html
@@ -7,12 +8,26 @@ from nltk.corpus import wordnet as wn
 S = wn.synset
 L = wn.lemma
 
+
 print(sys.argv)
-if len(sys.argv) < 4:
-    print("Usage: [input_file.csv] ")
+if len(sys.argv) < 5:
+    print("Usage: [input_file.csv] [output_file] [wordset_file] [stanfordNlp3.7_path]")
 input_file = sys.argv[1]
 output_file = sys.argv[2]
 wordset_file = sys.argv[3]
+stanfordNlpPath = sys.argv[4]
+nlp = StanfordCoreNLP(stanfordNlpPath)
+
+def lemma(text):
+    global nlp
+    props={'annotators': 'lemma','pipelineLanguage':'en','outputFormat':'json'}
+    r_dict = nlp.annotate(text, properties=props)
+    r_dict = yaml.safe_load(r_dict)
+    lemma = []
+    for s in r_dict['sentences']:
+        for token in s['tokens']:
+            lemma.append(token['lemma'])
+    return lemma
 
 def get_synonyms(term,pos='n'):
     synonym = set()
@@ -72,24 +87,24 @@ def get_siblings(term,pos='n'):
 word_set = set()
 
 ## matches:
-# 1. (.*) all string within parenthesis;
-rSpecial = re.compile(r"\(.*\)|[^a-zA-Z0-9- ]|- |- |\b\N\b")
+# 1. (.*) all string within parenthesis; # FIXME test only by now
+rSpecial = re.compile(r"\(.*\)|[^a-zA-Z0-9- ]|- |- |\b-\b")
 with open(input_file, 'rb') as csvfile:
     with open(output_file, 'w+') as of:
         reader = csv.reader(csvfile, delimiter=',', quotechar='"')
         writer = csv.writer(of,delimiter='\t',quotechar='"',lineterminator="\n")
-        writer.writerow(['word','cui','umls_syn','wn_syn','wn_ant','wn_hyper','wn_hypo','wn_holo','wn_mero','wn_sibling'])
+        writer.writerow(['word','word_norm','umls_syn','wn_syn','wn_ant','wn_hyper','wn_hypo','wn_holo','wn_mero','wn_sibling'])
         for row in reader:
-            if len(row) < 3: continue
+            if len(row) < 1: continue
             word = row[0]
             print("\n### word: %s" % word)
-            word_norm = rSpecial.sub('',word).strip()
+            word_norm = rSpecial.sub('',word).strip().replace(' ','_')
             word_set.add(word_norm)
-            cui = row[1]
-            synUmls = row[2].split(r'|')
+            cui = '' if len(row) < 2 else  row[1]
+            synUmls = [] if len(row) < 3 else  row[2].split(r'|')
             synUmlsSet = set()
             for s in synUmls:
-                s2 = rSpecial.sub('',s).lower().strip().replace(' ','_')
+                s2 = '_'.join(lemma(rSpecial.sub('',s).lower().strip()))
                 synUmlsSet.add(s2)
             print synUmlsSet
             synWnSet = get_synonyms(word_norm)
@@ -114,7 +129,7 @@ with open(input_file, 'rb') as csvfile:
             word_set |= siblings
             print(siblings)
 
-            writer.writerow([word,cui,'|'.join(synUmlsSet),'|'.join(synWnSet),'|'.join(antonyms),'|'.join(hypernym),\
+            writer.writerow([word,word_norm,'|'.join(synUmlsSet),'|'.join(synWnSet),'|'.join(antonyms),'|'.join(hypernym),\
                              '|'.join(hyponym),'|'.join(holonym),'|'.join(meronym),'|'.join(siblings)])
 
         with open(wordset_file, 'w+') as wf:
